e82c1a892edd962fa5117581fb692a24bc92806f
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-10-17 18:01:46 +0200

    Write tests for youtube download

--- test/test-youtube-search.js
+++ test/test-youtube-search.js
@@ -1,11 +1,31 @@
 var chai = require('chai');
-//var yt = require('youtube-search.js')
+var expect = chai.expect;
+var ytSearch = require('../src/youtube-search.js');
 
 describe('youtube-search.js', () => {
     describe('#search', () => {
-        it('should return given number of results');
-        it('should contain video id');
-        it('should contain video title');
-        it('should contain video thumbnail url');
+        var savedResult;
+        it('should return given number of results', done => {
+            ytSearch.search('Eminem', 3, (result) => {
+                try {
+                    savedResult = result;
+                    expect(result.length).to.equal(3);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+        it('should contain video id', () => {
+            expect(savedResult[0].hasOwnProperty('id')).to.be.true;
+        });
+
+        it('should contain video title', () => {
+            expect(savedResult[0].hasOwnProperty('title')).to.be.true;
+        });
+
+        it('should contain video thumbnails', () => {
+            expect(savedResult[0].hasOwnProperty('thumbnails')).to.be.true;
+        });
     });
 });
