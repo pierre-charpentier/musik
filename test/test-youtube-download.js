aec28656c4f7ad8d31514fbd1a85ec872629f1ce
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-10-17 17:06:22 +0200

    Add tests for youtube download and search modules

---
+++ test/test-youtube-download.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('youtube-download.js', () => {
+    describe('#download', () => {
+        it('should create a mp3 file named with the video id');
+        it('should create a non-empty file');
+    });
+});
---
+++ test/test-youtube-search.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+//var yt = require('youtube-search.js')
+
+describe('youtube-search.js', () => {
+    describe('#search', () => {
+        it('should return given number of results');
+        it('should contain video id');
+        it('should contain video title');
+        it('should contain video thumbnail url');
+    });
+});
