1b71dbfc611d601b0264beef83042de2df28c4b8
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-10-20 13:53:12 +0200

    Writing tests for youtube-download module

--- test/test-youtube-download.js
+++ test/test-youtube-download.js
@@ -1,9 +1,35 @@
+var ytDownload = require('../src/youtube-download');
 var chai = require('chai');
+var fs = require('fs');
+
 var assert = chai.assert;
 
 describe('youtube-download.js', () => {
     describe('#download', () => {
-        it('should create a mp3 file named with the video id');
-        it('should create a non-empty file');
+        var path = '../downloads/';
+        var videoId = 'lgT1AidzRWM';
+        it('should create a mp3 file named with the video id', done => {
+            ytDownload.download(videoId, () => {
+                fs.exists(path + videoId + '.mp3', (exists) => {
+                    try {
+                        expect(exists).to.be.true;
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+
+            });
+        });
+
+        it('should create a non-empty file', done => {
+            fs.stats(path + videoId + '.mp3', stats => {
+                try {
+                    expect(stats.size).to.be.above(0);
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
     });
 });
