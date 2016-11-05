dc2006eb355700adc1553ffd0b4fed4960b65300
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-11-05 19:02:08 +0100

    Use ffmpeg to convert video to music

--- src/youtube-download.js
+++ src/youtube-download.js
@@ -1,7 +1,22 @@
-var ytdl = require('ytdl-core');
+var CONFIG = require('../config.json'),
+    ytdl = require('ytdl-core'),
+    fs = require('fs'),
+    ffmpeg = require('fluent-ffmpeg');
 
 module.exports = exports = {};
 
-exports.download = (videoId, path, fileName) => {
-    
+exports.getSoundStream = (videoId, callback) => {
+    var stream = ytdl(CONFIG.youtubeBaseUrl + videoId);
+
+    proc = new ffmpeg({ source: stream });
+    proc.noVideo()
+        .format('mp3')
+        .on('end', function () {
+            console.log(Math.floor(Date.now() / 1000), videoId + ' - ' + fileName + ' SUCCESS');
+        })
+        .on('error', function (err) {
+            console.log('youtube-download.js #download -> ffmpeg "error" event: ' + err.message);
+        });
+
+    callback(proc);
 };
