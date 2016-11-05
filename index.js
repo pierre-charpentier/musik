10d90d8320406d3b2d62a83106604e9b43399d79
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-11-05 19:07:47 +0100

    Creating index.js and setting up express routes

---
+++ index.js
@@ -0,0 +1,30 @@
+var ytDownload = require('./src/youtube-download');
+var ytSearch = require('./src/youtube-search');
+var express = require('express');
+var app = express();
+var fs = require ('fs')
+var ffmpeg = require('fluent-ffmpeg');
+
+app.use(express.static('public'));
+
+app.get('/download/:videoId', (req, res) => {
+    ytDownload.download(req.params.videoId, req.params.videoId, (proc) => {
+        res.attachment(req.params.videoId + '.mp3');
+        proc.writeToStream(res, {end: true});
+    });
+});
+
+app.get('/search/:keywords', (req, res) => {
+    ytSearch.search(req.params.keywords, 9, (error, items) => {
+        if(error) {
+            res.end("Couldn't retrieve items.");
+        } else {
+            res.type("json");
+            res.end(JSON.stringify(items));
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Listening!");
+});
