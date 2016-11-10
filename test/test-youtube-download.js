ab8e9b2169c4773d0200c1f7a2e8a1c65552c2c4
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-11-10 17:03:05 +0100

    Rewrite youtube-download test file

--- test/test-youtube-download.js
+++ test/test-youtube-download.js
@@ -1,31 +1,18 @@
 var ytDownload = require('../src/youtube-download');
 var chai = require('chai');
+var expect = chai.expect;
 var fs = require('fs');
 
 var assert = chai.assert;
 
 describe('youtube-download.js', () => {
-    describe('#download', () => {
-        var path = '../downloads/';
+    describe('#getSoundStream', () => {
         var videoId = 'lgT1AidzRWM';
-        it('should create a mp3 file named with the video id', done => {
-            ytDownload.download(videoId, path, videoId, () => {
-                fs.exists(path + videoId + '.mp3', (exists) => {
-                    try {
-                        expect(exists).to.be.true;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
-
-            });
-        });
-
-        it('should create a non-empty file', done => {
-            fs.stats(path + videoId + '.mp3', stats => {
+        it('should return a stream', done => {
+            ytDownload.getSoundStream(videoId, (stream) => {
                 try {
-                    expect(stats.size).to.be.above(0);
+                    expect(typeof stream.on === 'function').to.be.true;
+                    done();
                 } catch (e) {
                     done(e);
                 }
