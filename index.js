4abaaea59be60db3fe194b9e329f1cf1f4a0640e
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-03-20 19:28:46 +0100

    Add dotenv and rework index.js file

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-var ytDownload = require('./src/youtube-download');
-var ytSearch = require('./src/youtube-search');
-var express = require('express');
-var app = express();
-var fs = require ('fs')
-var ffmpeg = require('fluent-ffmpeg');
-var cors = require('cors');
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-app.use(cors());
+const youtubeDownload = require('./src/youtube-download')
+const youtubeSearch = require('./src/youtube-search')
+
+app.set('port', process.env.MUSIK_PORT)
+
+app.use(express.static('public'))
 
 app.get('/download/:videoId', (req, res) => {
-    ytDownload.getSoundStream(req.params.videoId, (soundStream, videoTitle) => {
-        res.attachment(videoTitle + '.mp3');
-        soundStream.writeToStream(res, {end: true});
-    });
-});
+  youtubeDownload.getSoundStream(req.params.videoId, (soundStream, videoTitle) => {
+    res.attachment(`${videoTitle}.mp3`)
+
+    soundStream.writeToStream(res, {
+      end: true
+    })
+  })
+})
 
 app.get('/search/:keywords', (req, res) => {
-    ytSearch.search(req.params.keywords, 9, (error, items) => {
-        if(error) {
-            res.end("Couldn't retrieve items.");
-        } else {
-            res.type("json");
-            res.end(JSON.stringify(items));
-        }
-    });
-});
+  youtubeSearch.search(req.params.keywords, 9, (error, items) => {
+    if (error) {
+      res.end('Couldn\'t retrieve items.')
+    } else {
+      res.type('json')
+      res.end(JSON.stringify(items))
+    }
+  })
+})
 
 app.listen(3000, () => {
-    console.log("Listening!");
-});
+  console.log('Listening!')
+})
